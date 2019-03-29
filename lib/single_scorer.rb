39f35b343ab5a04f4b743cd0c1311a7ee5ef8b92
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-03-29 12:44:37 -0400

    Calculate score for open frame

--- lib/single_scorer.rb
+++ lib/single_scorer.rb
@@ -1,7 +1,18 @@
 class SingleScorer
-   
+
+  def initialize
+    @scores = [nil] * 10    
+  end
   def scores
-    [nil] * 10
+    @scores
+  end
+
+  def annotate(fallen_pins)
+    if @scores[0] == nil
+      @scores[0] = fallen_pins 
+    else
+      @scores[0] += fallen_pins 
+    end
   end
   
 end
--- spec/single_scorer_spec.rb
+++ spec/single_scorer_spec.rb
@@ -1,11 +1,21 @@
 require 'single_scorer'
 
 RSpec.describe SingleScorer do
+
+  before(:each) do
+    @scorer = SingleScorer.new
+  end
   
   it 'initializes with a list of empty scores' do
-    game = SingleScorer.new
-    scores = game.scores
-    expect(scores).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil, nil])
+    expect(@scorer.scores)
+      .to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil, nil])
+  end
+
+  it 'calculates 7 points by falling 3 and 4 pins' do
+    @scorer.annotate(3)
+    @scorer.annotate(4)
+    expect(@scorer.scores)
+      .to eq([7, nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
 end
