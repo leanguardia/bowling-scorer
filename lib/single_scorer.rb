aa11b11c6cc28ffab833389d72f12802408b1d08
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-03-30 10:57:44 -0400

    Refactor: remove duplicated code

--- lib/single_scorer.rb
+++ lib/single_scorer.rb
@@ -11,68 +11,52 @@ class SingleScorer
   end
 
   def annotate(fallen_pins)
-    if first_roll?
+    if is_new_frame?
       @current_frame = Frame.new(fallen_pins)
-      if @current_frame.is_strike?
-        annotate_current_frame(10)
-        if last_frame
-          if last_frame.is_strike?
-            add_to_previous_frame(10)
-            if second_last_frame && second_last_frame.is_strike?
-              @scores[frames_count - 2] += 10
-            end
-          elsif last_frame.is_spare?
-            add_to_previous_frame(@current_frame.first_roll)
-          end
-        end
-        close_frame
-      end
     else
       @current_frame.second_roll = fallen_pins
-      annotate_current_frame(@current_frame.points)
-      if last_frame
-        if last_frame.is_strike?
-          add_to_previous_frame(@current_frame.points)
-          if second_last_frame && second_last_frame.is_strike?
-            @scores[frames_count - 2] += 10
-          end
-        elsif last_frame.is_spare?
-          add_to_previous_frame(@current_frame.first_roll)
-        end
-      end
-      close_frame
+    end
+    if @current_frame.is_complete?
+      @scores[frames_count] = @current_frame.points
+      check_bonus_points
+      complete_frame
     end
   end
 
 private
+
+  def is_new_frame?
+    @current_frame == nil
+  end
 
   def frames_count
     @frames.size
   end
 
   def last_frame
-    @frames.last
+    @frames[-1]
   end
 
   def second_last_frame
     @frames[-2]
   end
 
-  def first_roll?
-    @current_frame == nil
-  end
-
-  def close_frame
+  def complete_frame
     @frames.push(@current_frame)
     @current_frame = nil
   end
 
-  def annotate_current_frame(points)
-    @scores[frames_count] = points
-  end
-
-  def add_to_previous_frame(points)
-    @scores[frames_count - 1] += points
+  def check_bonus_points
+    if last_frame
+      if last_frame.is_strike?
+        @scores[frames_count - 1] += @current_frame.points
+        if second_last_frame && second_last_frame.is_strike?
+          @scores[frames_count - 2] += 10
+        end
+      elsif last_frame.is_spare?
+        @scores[frames_count - 1] += @current_frame.first_roll
+      end
+    end
   end
 
 end
