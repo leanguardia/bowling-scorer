66100680c27332c17f6264aeb3f2db2c0640e775
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-03-30 11:19:19 -0400

    Refactor: simplify tests setup

--- spec/single_scorer_spec.rb
+++ spec/single_scorer_spec.rb
@@ -12,33 +12,25 @@ RSpec.describe SingleScorer do
   end
 
   it 'calculates 7 points by falling 3 and 4 pins' do
-    @scorer.annotate(3)
-    @scorer.annotate(4)
+    annotateAll(3, 4)
     expect(@scorer.scores)
       .to eq([7, nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 9 points by falling 5 and 4 pins on the second frame' do
-    @scorer.annotate(3)
-    @scorer.annotate(4)
-    @scorer.annotate(5)
-    @scorer.annotate(4)
+    annotateAll(3, 4, 5, 4)
     expect(@scorer.scores)
       .to eq([7, 9, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 10 points by falling 3 and 7 pins (spare)' do
-    @scorer.annotate(3)
-    @scorer.annotate(7)
+    annotateAll(3, 7)
     expect(@scorer.scores)
       .to eq([10, nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 16 points after a spare and falling 6 pins' do
-    @scorer.annotate(3)
-    @scorer.annotate(7)
-    @scorer.annotate(6)
-    @scorer.annotate(3)
+    annotateAll(3, 7, 6, 3)
     expect(@scorer.scores)
       .to eq([16, 9, nil, nil, nil, nil, nil, nil, nil, nil])
   end
@@ -50,60 +42,43 @@ RSpec.describe SingleScorer do
   end
 
   it 'calculates 18 points after a strike and falling 3 and 5 pins' do
-    @scorer.annotate(10)
-    @scorer.annotate(3)
-    @scorer.annotate(5)
+    annotateAll(10, 3, 5)
     expect(@scorer.scores)
       .to eq([18, 8, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
-  it 'calculates 20 points on a spare frame after a consecutive strike' do
-    @scorer.annotate(5)
-    @scorer.annotate(5)
-    @scorer.annotate(10)
+  it 'calculates 20 points on a spare frame after a strike' do
+    annotateAll(5, 5, 10)
     expect(@scorer.scores)
       .to eq([20, 10, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 20 points on the first frame after two strikes' do
-    @scorer.annotate(10)
-    @scorer.annotate(10)
+    annotateAll(10, 10)
     expect(@scorer.scores)
       .to eq([20, 10, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 30 and 20 points on the first two frames after two strikes and a spare' do
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(9)
-    @scorer.annotate(1)
+    annotateAll(10, 10, 9, 1)
     expect(@scorer.scores)
       .to eq([30, 20, 10, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'calculates 30 and 20 points on the first two frames after three strikes' do
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
+    annotateAll(10, 10, 10)
     expect(@scorer.scores)
       .to eq([30, 20, 10, nil, nil, nil, nil, nil, nil, nil])
   end
   
   it 'calculates scores for a perfect game' do
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
-    @scorer.annotate(10)
+    annotateAll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
     expect(@scorer.scores)
       .to eq([30, 30, 30, 30, 30, 30, 30, 30, 30, 30])
   end
 
+end
+
+def annotateAll(*fallen_pins_list)
+  fallen_pins_list.each { |pins| @scorer.annotate(pins) }
 end
