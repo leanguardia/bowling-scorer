e7390d15b75685ed4c0ea5463c4dde8a62dba34d
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-03-31 12:32:39 -0400

    Display empty board for many players

--- lib/bowling_scorer.rb
+++ lib/bowling_scorer.rb
@@ -1,14 +1,21 @@
 class BowlingScorer
 
+  def initialize
+    @players = []
+  end
+
   def add_player(player_name)
-    @player = 'Alicia'
+    @players.push(player_name)
   end
 
   def display
-    "Frame\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n"+
-    "Alice\n"+
-    "Pinfalls\t\n"+
-    "Score\t\n"
+    board = "Frame\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n"
+    @players.each do |player_name| 
+      board += "#{player_name}\n"+
+               "Pinfalls\t\n"+
+               "Score\t\n"
+    end
+    board
   end
 
 end
--- spec/bowling_scorer_spec.rb
+++ spec/bowling_scorer_spec.rb
@@ -1,15 +1,34 @@
 require 'bowling_scorer'
 
 RSpec.describe BowlingScorer do
-  
+
+  before(:each) do
+    @scorer = BowlingScorer.new
+    @scorer.add_player('Alice')
+  end
+
   it 'displays empty scoreboard for one player' do
-    scorer = BowlingScorer.new
-    scorer.add_player('Alice')
-    expect(scorer.display)
-    .to eq ("Frame\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n"+
-      "Alice\n"+
-      "Pinfalls\t\n"+
-      "Score\t\n")
+    board = "Frame\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n"+
+            "Alice\n"+
+            "Pinfalls\t\n"+
+            "Score\t\n"
+    expect(@scorer.display).to eq (board)
+  end
+
+  it 'displays empty scoreboard for many players' do
+    @scorer.add_player('Bob')
+    @scorer.add_player('Camila')
+    board = "Frame\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n"+
+            "Alice\n"+
+            "Pinfalls\t\n"+
+            "Score\t\n"+
+            "Bob\n"+
+            "Pinfalls\t\n"+
+            "Score\t\n"+
+            "Camila\n"+
+            "Pinfalls\t\n"+
+            "Score\t\n"
+    expect(@scorer.display).to eq (board)
   end
     
 end
