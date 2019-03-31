973ec44290f67c089765e107accd01c19934b106
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-03-31 17:27:05 -0400

    Split tests: display special frames

--- spec/bowling_scorer_spec.rb
+++ spec/bowling_scorer_spec.rb
@@ -15,18 +15,32 @@ RSpec.describe BowlingScorer do
     expect(@scorer.display).to eq (board)
   end
 
-  it 'updates pinfalls and score after a frame' do
+  it 'updates board after an open frame' do
     @scorer.annotate('Alice', 4,5);
     score = "Pinfalls\t4\t5\n"+
             "Score\t\t9\n"
     expect( extract_score(@scorer.display) ).to eq (score)
   end
 
-  it 'updates pinfalls and score after a spare an open frame and a strike' do
-    @scorer.annotate('Alice', 5,5, 3,4, 10);
-    score = "Pinfalls\t5\t/\t3\t4\t\tX\n"+
-            "Score\t\t13\t7\t10\n"
-    expect( extract_score(@scorer.display)).to eq (score)
+  it 'updates board after spare and open frame' do
+    @scorer.annotate('Alice', 5,5, 3,4);
+    score = "Pinfalls\t5\t/\t3\t4\n"+
+            "Score\t\t13\t7\n"
+    expect( extract_score(@scorer.display) ).to eq (score)
+  end
+
+  it 'updates board after strike and open frame' do
+    @scorer.annotate('Alice', 5,5, 3,4);
+    score = "Pinfalls\t5\t/\t3\t4\n"+
+            "Score\t\t13\t7\n"
+    expect( extract_score(@scorer.display) ).to eq (score)
+  end
+
+  it 'updates board after fouls' do
+    @scorer.annotate('Alice', 'F',5, 3,'F', 'F','F');
+    score = "Pinfalls\tF\t5\t3\tF\tF\tF\n"+
+            "Score\t\t5\t3\t0\n"
+    expect( extract_score(@scorer.display) ).to eq (score)
   end
 
   it 'displays empty scoreboard for many players' do
